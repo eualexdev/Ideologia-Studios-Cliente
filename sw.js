f1e00c302f9664388ff07b7672f2e5c9681036fc
eualexdev <eujosealexsandro@gmail.com>
2021-03-05 18:26:32 -0300

    Pwa Configurado para OffLine

--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
-self.addEventListener('fetch', () => {
+var cacheName = 'ISTudios';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/img/Logo-256x256.png',
+    './sw.js',
+    './login/index.html',
+    './login/main.css',
+    './login/config.js',
+    './alunos/index.html',
+    './alunos/src/index.js',
+    './alunos/src/style/index.css',
+    './alunos/src/style/menuBar.css'
+    // adiciona todos os arquivos
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-  
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
