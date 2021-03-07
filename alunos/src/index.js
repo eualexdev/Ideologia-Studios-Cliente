b7c27511741aa523dfbd23cad8c721312875fe22
eualexdev <eujosealexsandro@gmail.com>
2021-03-07 10:01:24 -0300

    Menu de cores quase feito

--- alunos/src/index.js
+++ alunos/src/index.js
@@ -1,6 +1,24 @@
 var User = String(localStorage.getItem("nome")).split(' ')[0]
 var Instrumento = String(localStorage.getItem("instrumento"))
 
-
 document.querySelector("h2#name-text").innerHTML = "Olá, " + User + "!"
 // document.querySelector("h2#name-instrumento").innerHTML = Instrumento
+
+
+let pencilButton = document.querySelector("i.pencil")
+let menuColor = document.getElementById("color-menu")
+let closeColorMenuButton = document.getElementById("close-menu-color")
+
+pencilButton.addEventListener(
+    'click',
+    () => {
+        menuColor.style.top = "0%"
+    }
+)
+
+closeColorMenuButton.addEventListener(
+    "click",
+    () => {
+        menuColor.style.top = "-100%"
+    }
+)
